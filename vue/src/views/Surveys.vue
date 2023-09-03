54365460d93f7569c129bf716c7cdbcc53045daf
ranaarrouk <ranaarrouk@gmail.com>
2023-09-03 23:09:08 +0800

    Create reusable PageComponent

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,16 +1,16 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      This is my all surveys
-    </div>
-  </main>
+  <PageComponent title="Surveys">My Surveyssssssssssss</PageComponent>
 </template>
 
-<script setup>
+<script>
 
+  import PageComponent from "../components/PageComponent.vue";
+
+  export default {
+    components: {PageComponent}
+  }
 </script>
+
+<style scoped>
+
+</style>
