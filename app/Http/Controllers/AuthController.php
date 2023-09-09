f42934f08c0b5c67d542f49dbe1235b62190f851
ranaarrouk <ranaarrouk@gmail.com>
2023-09-09 19:33:15 +0800

    Implement login with error msg, init axios

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
@@ -32,8 +33,32 @@ class AuthController extends Controller
         $token = $user->createToken('main')->plainTextToken;
 
         return response([
-           'user' => $user,
-           'token' => $token,
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email|string|exists:users,email',
+            'password' => 'required',
+            'remember' => 'boolean',
+        ]);
+
+        $remember = $credentials['remember'] ? $credentials['remember'] : false;
+        unset($credentials['remember']);
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'validation error'], 422);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
         ]);
     }
 }
