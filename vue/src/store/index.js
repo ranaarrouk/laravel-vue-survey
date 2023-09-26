d0f66c45e6261e8e20eac4610517849adf6f4655
ranaarrouk <ranaarrouk@gmail.com>
2023-09-26 22:00:24 +0800

    Get all surveys from database, create SurveyListItem component

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,128 +1,6 @@
 import {createStore} from "vuex";
 import axiosClient from "../axios";
 
-const tmpSurveys = [
-  {
-    id: 1,
-    title: "First survey",
-    slug: "first-survey-1",
-    status: "draft",
-    image: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQxMDMtbWlzY3RoZW1ldDAwMTQ4YS1pbWFnZS5wbmc.png",
-    description: "First survey about me, Rana Arouk First survey about me, Rana AroukFirst survey about me, Rana AroukFirst survey about me, Rana AroukFirst survey about me, Rana AroukFirst survey about me, Rana AroukFirst survey about me, Rana AroukFirst survey about me, Rana AroukFirst survey about me, Rana AroukFirst survey about me, Rana Arouk",
-    created_at: '2023-09-07 00:00:00',
-    updated_at: '2023-09-07 00:00:00',
-    expire_date: '2023-09-07 00:00:00',
-    questions: [
-      {
-        id: 1,
-        type: "select",
-        question: "from Which country are you ?",
-        description: null,
-        data: {
-          options: [
-            {uuid: "9855989798", text: "Syria"},
-            {uuid: "944989798", text: "Canada"},
-            {uuid: "339989798", text: "US"},
-            {uuid: "985489798", text: "UK"},
-          ],
-        }
-      },
-      {
-        id: 2,
-        type: "checkbox",
-        question: "What are the programming languages you have experience with?",
-        description: "test test test",
-        data: {
-          options: [
-            {uuid: "98559897", text: "PHP"},
-            {uuid: "9449897", text: "C#"},
-            {uuid: "3399897", text: "JAVA"},
-            {uuid: "9854897", text: "C++"},
-          ],
-        }
-      },
-      {
-        id: 3,
-        type: "checkbox",
-        question: "What are the web frameworks have you worked on ?",
-        description: "frameworks",
-        data: {
-          options: [
-            {uuid: "98559798", text: "Laravel"},
-            {uuid: "9449798", text: "ASP.Net"},
-            {uuid: "3399798", text: "Django"},
-            {uuid: "9854798", text: "Codeignitor"},
-          ],
-        }
-      },
-      {
-        id: 4,
-        type: "radio",
-        question: "Do you speak english well?",
-        description: "english",
-        data: {
-          options: [
-            {uuid: "9855798", text: "Yes"},
-            {uuid: "944998", text: "No"},
-          ],
-        }
-      },
-      {
-        id: 5,
-        type: "checkbox",
-        question: "What are the tools have you worked ?",
-        description: "tools",
-        data: {
-          options: [
-            {uuid: "98559798", text: "PHPStorm"},
-            {uuid: "9449798", text: "Navicat"},
-            {uuid: "3399798", text: "GitKraken"},
-            {uuid: "9854798", text: "Xampp"},
-          ],
-        }
-      },
-      {
-        id: 6,
-        type: "text",
-        question: "Your email",
-        description: "HR",
-        data: {}
-      },
-      {
-        id: 7,
-        type: "textarea",
-        question: "Talk about yourself?",
-        description: null,
-        data: {}
-      },
-    ],
-  },
-  {
-    id: 200,
-    title: "Second survey",
-    slug: "second-survey-2",
-    status: "draft",
-    image: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjgyMC1rYXRpZS01NC5wbmc.png",
-    description: "Second survey about learning english, Second survey about learning english Second survey about learning english Second survey about learning english Second survey about learning english",
-    created_at: '2023-09-07 00:00:00',
-    updated_at: '2023-09-07 00:00:00',
-    expire_date: '2023-09-07 00:00:00',
-    questions: []
-  },
-  {
-    id: 300,
-    title: "Third survey",
-    slug: "third-survey-3",
-    status: "draft",
-    image: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjUzNy1udW5vb24tMDMucG5n.png",
-    description: "Third survey about atomic habits, survey about atomic habits, survey about atomic habits, survey about atomic habits ",
-    created_at: '2023-09-07 00:00:00',
-    updated_at: '2023-09-07 00:00:00',
-    expire_date: '2023-09-07 00:00:00',
-    questions: []
-  }
-];
-
 const store = createStore({
   state: {
     user: {
@@ -133,7 +11,10 @@ const store = createStore({
       loading: false,
       data: {},
     },
-    surveys: [...tmpSurveys],
+    surveys: {
+      loading: false,
+      data: []
+    },
     questionTypes: ["text", "select", "radio", "checkbox", "textarea"]
   },
   getters: {},
@@ -179,8 +60,7 @@ const store = createStore({
       }
       return response;
     },
-    getSurvey({commit}, id)
-    {
+    getSurvey({commit}, id) {
       commit('setCurrentSurveyLoading', true);
       return axiosClient.get(`/survey/${id}`)
         .then((res) => {
@@ -188,14 +68,23 @@ const store = createStore({
           commit('setCurrentSurveyLoading', false);
           return res;
         }).catch((err) => {
-        commit('setCurrentSurveyLoading', false);
-        throw err;
-      });
+          commit('setCurrentSurveyLoading', false);
+          throw err;
+        });
     },
-    deleteSurvey({}, id)
-    {
+    deleteSurvey({}, id) {
       return axiosClient.delete(`/survey/${id}`);
-    }
+    },
+
+    getSurveys({commit}) {
+      commit('setSurveysLoading', true);
+      return axiosClient.get('/survey')
+        .then((res) => {
+          commit('setSurveysData', res.data);
+          commit('setSurveysLoading', false);
+          return res;
+        })
+    },
   },
   mutations: {
     logout: (state) => {
@@ -209,10 +98,16 @@ const store = createStore({
     },
     setCurrentSurveyLoading: (state, loading) => {
       state.currentSurvey.loading = loading;
-    } ,
+    },
     setCurrentSurveyData: (state, survey) => {
       state.currentSurvey.data = survey.data;
-    }
+    },
+    setSurveysLoading: (state, loading) => {
+      state.surveys.loading = loading;
+    },
+    setSurveysData: (state, surveys) => {
+      state.surveys.data = surveys.data;
+    },
   },
   modules: {},
 });
